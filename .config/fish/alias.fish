91927e4231d33d5d229c837a4a003a8df7acf015
NuxDD <maxime.desousa31@gmail.com>
2024-08-08 18:32:27 +0000

    add fish aliases

---
+++ .config/fish/alias.fish
@@ -0,0 +1,12 @@
+# Git related 
+alias gs="git status"
+alias ga="git add"
+alias gd="git diff"
+alias gc="git commit -m"
+
+# Paths related
+alias ..="cd ../"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+alias ll="ls -al"
---
+++ .config/fish/config.fish
@@ -0,0 +1,3 @@
+set fish_greeting ""
+
+source $HOME/.config/fish/alias.fish
