26f11caa17fbf88f74ac5fa01e7b670541d1656b
NuxDD <maxime.desousa31@gmail.com>
2024-08-15 19:20:02 +0000

    add nvim config: harpoon (add tabline)

--- .config/nvim/lua/user/harpoon.lua
+++ .config/nvim/lua/user/harpoon.lua
@@ -1,2 +1,14 @@
+local status_ok, harpoon = pcall(require, "harpoon")
+if not status_ok then
+	return
+end
+
 mark = require("harpoon.mark")
 ui = require("harpoon.ui")
+
+harpoon.setup({
+	tabline = true,
+	menu = {
+		width = vim.api.nvim_win_get_width(0) - 20,
+	},
+})
