68a7cc650b5130507d41833bbba17ff14dacfba1
NuxDD <maxime.desousa31@gmail.com>
2024-08-15 19:29:10 +0000

    add nvim config: harpoon tabline colors

--- .config/nvim/lua/user/harpoon.lua
+++ .config/nvim/lua/user/harpoon.lua
@@ -12,3 +12,9 @@ harpoon.setup({
 		width = vim.api.nvim_win_get_width(0) - 20,
 	},
 })
+
+vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
+vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
