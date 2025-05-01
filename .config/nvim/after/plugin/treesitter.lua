72d659d144d15785aba05e9bf29c2bec4cc1da8a
MDS <maxime.desousa31@gmail.com>
2025-05-02 00:56:13 +0200

    nvim: add rust default treesitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'lua', 'python', 'typescript', 'html', 'css', 'javascript' },
+    ensure_installed = { 'lua', 'python', 'rust' },
     auto_install = false,
     highlight = { enable = true },
     indent = { enable = true },
